1db01f1d06659ea4399bf9a4a7348ef3b7e1b41a
Richie Z <122392615+richie119@users.noreply.github.com>
2024-03-30 00:07:38 -0400

    Add files via upload

---
+++ Foundations _ Dynamic Memory_files/workshop_2.js
@@ -0,0 +1,3 @@
+document.write('<a href="../../dynamic/workshops/w2.html">');
+document.write("Dynamic&nbsp;Memory");
+document.write("</a>");
---
+++ Foundations _ Member Functions and Privacy_files/workshop_3.js
@@ -0,0 +1,3 @@
+document.write('<a href="../../dynamic/workshops/w3.html">');
+document.write("Classes&nbsp;and&nbsp;Privacy");
+document.write("</a>");
